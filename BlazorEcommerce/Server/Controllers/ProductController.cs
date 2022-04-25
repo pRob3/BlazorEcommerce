0f55743324d4303bb0ef534bbd40c07fabd5c735
pRob3 <bubblan@slaf.se>
2022-04-25 18:46:08 +0200

    Add categories and seeding

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -17,10 +17,14 @@ namespace BlazorEcommerce.Server.Controllers
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
             var result = await _productService.GetProductsAsync();
-
             return Ok(result);
         }
 
-
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        }
     }
 }
--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -9,5 +9,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
     public interface IProductService
     {
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
+
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
     }
 }
